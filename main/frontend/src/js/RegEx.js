a1a261e1d4a11484c114879a7571aecaa5d87585
Sathish B <146500720+SathishB2003@users.noreply.github.com>
2024-11-18 21:30:25 +0530

    Delete main directory

--- main/frontend/src/js/RegEx.js
+++
@@ -1,3 +0,0 @@
-export const validName = new RegExp('^[a-zA-Z ]+$');
-export const validEmail = new RegExp('^[a-zA-Z0-9]+@(?:[a-zA-Z0-9]+.)+[A-Za-z]+$');
-export const validPassword = new RegExp('^(?=.*?[A-Za-z])(?=.*?[0-9]).{8,12}$');
